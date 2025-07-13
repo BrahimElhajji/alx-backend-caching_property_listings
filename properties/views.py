9493289cef0afe759592a1f81f9d20c2c82ab411
BrahimElhajji <elhajji080898@gmail.com>
2025-07-13 17:54:26 +0000

    fixed

--- properties/views.py
+++ properties/views.py
@@ -6,6 +6,6 @@ from .models import Property
 @cache_page(60 * 15)  # Cache for 15 minutes
 def property_list(request):
     properties = Property.objects.all().values()
-    return JsonResponse(list(properties), safe=False)
+    return JsonResponse({"data": list(properties)})
 
 # Create your views here.
